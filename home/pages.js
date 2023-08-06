b978e27143d63de17024e828886e65a2efebcc80
Skumar1428 <141502817+Skumar1428@users.noreply.github.com>
2023-08-06 14:56:47 +0530

    Update pages.js

--- home/pages.js
+++ home/pages.js
@@ -1,6 +1,6 @@
 const ghpages = require("gh-pages");
 const pathname = `${__dirname}/build`;
-const repoURL = "https://github.com/hashirshoaeb/hashirshoaeb.github.io.git";
+const repoURL = "https://github.com/Skumar1428/Skumar1428.github.io.git";
 
 ghpages.publish(
   pathname,
